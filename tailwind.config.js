91274942652b606a30ff75c5f2cf459d73105093
unknown <mfaqihuddin23@gmail.com>
2022-07-18 17:10:18 +0700

    preparing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html, ./assets/**/*.{html,js}'],
+  content: ['./index.html', ' ./assets/**/*.{html,js,}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins:['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'imcolor': {
+          100 : '#2B2B2B',
+          200 : 'rgba(43, 43, 43, 0.45)',
+          300 : '#8B74EB',
+        },
+      },
+      container: {
+        center: true,
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
