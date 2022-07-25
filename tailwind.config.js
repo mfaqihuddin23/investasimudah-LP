698d86811d061f88370049150a3c019aadedada8
unknown <mfaqihuddin23@gmail.com>
2022-07-25 15:53:52 +0700

    all done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ module.exports = {
         poppins:['Poppins', 'sans-serif'],
       },
       colors: {
-        'imcolor': {
+        'imcol': {
           100 : '#2B2B2B',
           200 : 'rgba(43, 43, 43, 0.45)',
           300 : '#8B74EB',
+          400 : '#FFA4DA',
+          500 : 'rgba(255, 255, 255, 0.7)',
+          600 : '#121212',
+        },
+        'imgrad': {
+          100 : '#524FD0',
+          200 : '#B0AEFB',
+          300 : '#AAA8FA',
+          
         },
       },
       container: {
